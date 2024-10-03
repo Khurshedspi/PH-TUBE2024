f071ea85b35391cd67a6d2228da8a2c5e29381b6
Khurshedspi <khurshed5322@gmail.com>
2024-10-03 11:08:05 +0600

    added video title, profile, name, description

--- js/video.js
+++ js/video.js
@@ -21,16 +21,17 @@ fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
 const displayVideos = (videos) =>{
     const videosContainer = document.getElementById('videos');
    videos.forEach((video) =>{
-    console.log(video);
+   
     const card = document.createElement( 'div');
     card.classList = 'card card-compact';
     card.innerHTML = `
     
- <figure class ="h-[200px]">
+ <figure class ="h-[200px] relative">
     <img
       src= ${video.thumbnail}
       class ="h-full w-full object-cover"
       alt="video" />
+      <span class ="absolute right-2 bottom-2 text-white bg-black rounded p-1">${video.others.posted_date}</span>
   </figure>
   <div class="px-0 py-3 flex gap-2">
 
@@ -41,7 +42,7 @@ const displayVideos = (videos) =>{
 <h2 class ="font-bold">${video.title}</h2>
 <div class ="flex items-center gap-2">
 <p class ="text-gray-500">${video.authors[0].profile_name}</p>
-<img class ="h-4 w-4 " src ="https://img.icons8.com/?size=100&id=D9RtvkuOe31p&format=png&color=000000" />
+${video.authors[0].verified === true ? `<img class ="h-4 w-4 " src ="https://img.icons8.com/?size=100&id=D9RtvkuOe31p&format=png&color=000000" />`: ''}
 </div>
 <p class ="text-gray-500">Views: ${video.others.views}  </p>
   </div>
@@ -57,13 +58,10 @@ const displayVideos = (videos) =>{
 const displayCategories = (categories) => {
     const categoryContainer = document.getElementById('categories');
     categories.forEach((item) => {
-        
-
         // create a button
         const button = document.createElement('button');
         button.classList = 'btn';
         button.innerText = item.category;
-
         // add button to category container 
         categoryContainer.append(button);
     })
